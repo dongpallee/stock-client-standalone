99f8d608c2231e7ddc011bdbe5778aae494b0a84
dundunstep <dundunstep@gmail.com>
2025-12-22 20:26:41 +0900

    Update ProtectedRoute.jsx
    pathname+search+hash 만 저장

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -21,7 +21,9 @@ const ProtectedRoute = ({ children }) => {
   if (!user) {
     // 로그인하지 않은 사용자는 로그인 페이지로 리다이렉트
     // 현재 위치를 state로 저장하여 로그인 후 원래 페이지로 돌아갈 수 있도록 함
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    const from = location.pathname + location.search + location.hash;
+    return <Navigate to="/login" state={{ from }} replace />;
+
   }
 
   return <>{children}</>;
