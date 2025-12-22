904c3d90b2d054c346b24c11c51f8b2d4ccbf3f8
dundunstep <dundunstep@gmail.com>
2025-12-22 20:28:53 +0900

    Update RegisterForm.jsx
    handleChange는 함수형 업데이트

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -24,10 +24,11 @@ const RegisterForm = () => {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   const validateForm = () => {
