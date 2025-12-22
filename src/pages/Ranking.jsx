a990a3345bd3d937b2f559e3ab1039b269c37569
winteringhoy <winteringhoy@gmail.com>
2025-12-22 16:01:38 +0900

    Update Ranking.jsx
    미사용 import 제거

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -8,7 +8,6 @@ import { Badge } from '../components/ui/badge';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs';
 import { 
   Trophy, 
-  TrendingUp, 
   BarChart3, 
   Newspaper,
   Calculator,
