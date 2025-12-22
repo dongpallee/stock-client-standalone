47a4bb393dd1669d4040e8accaeeb24f1737d6c7
dundunstep <dundunstep@gmail.com>
2025-12-22 20:24:04 +0900

    Update LoginForm.jsx
    오타 수정

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -78,7 +78,7 @@ const LoginForm = () => {
         );
       }
     } catch (err) {
-      setError('일시적인 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.'
+      setError('일시적인 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.')
     } finally {
       setLoading(false);
     }
